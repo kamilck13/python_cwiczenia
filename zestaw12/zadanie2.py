32839159a716ae3d4e5dae5b52d77234e56bac3e
Agnieszka Cwiekowska <agnieszka.cwiekowska@uj.edu.pl>
2019-01-14 22:16:15 +0100

    niewielkie zmiany

--- zestaw12/zadanie2.py
+++ zestaw12/zadanie2.py
@@ -1,36 +1,55 @@
 #12.2
 import unittest
 
-def czy_posortowane(L):
-    for i in range(1, len(L)):
-        if(L[i] < L[i-1]):
-            return False
-    return True   
-
 def binarne_rek(L, left, right, y):
-    if not czy_posortowane(L):
-        raise AssertionError("Elementy nie są posortowane (uporządkowane)")
-    mid = int((left + right)/2)
-    if(L[mid] < y):
-        return binarne_rek(L, mid+1, right, y)
-    elif(L[mid] > y):
-        return binarne_rek(L, left, mid-1, y)
-    else:
+    mid = int((left + right) / 2)
+    if L[mid] == y:
         return mid
-    return None
-    
+    elif (L[mid] < y and mid != right):
+        return binarne_rek(L, mid + 1, right, y)
+    elif (L[mid] > y and mid != left):
+        return binarne_rek(L, left, mid - 1, y)
+    else:
+        return None
+
 
 class AllTests(unittest.TestCase):
     def test1(self):
         L = (1, 2, 3, 4, 5, 6, 7)
-        self.assertEqual(binarne_rek(L, 0, 6, 5), 4)
+        self.assertEqual(binarne_rek(L, 0, 6, 0), None)
+
     def test2(self):
         L = (1, 2, 3, 4, 5, 6, 7)
-        self.assertEqual(binarne_rek(L, 0, 4, 2), 1)
+        self.assertEqual(binarne_rek(L, 0, 6, 1), 0)
+
     def test3(self):
         L = (1, 2, 3, 4, 5, 6, 7)
-        self.assertEqual(binarne_rek(L, 3, 7, 4), 3)
-        
+        self.assertEqual(binarne_rek(L, 0, 6, 2), 1)
+
+    def test4(self):
+        L = (1, 2, 3, 4, 5, 6, 7)
+        self.assertEqual(binarne_rek(L, 0, 6, 3), 2)
+
+    def test5(self):
+        L = (1, 2, 3, 4, 5, 6, 7)
+        self.assertEqual(binarne_rek(L, 0, 6, 4), 3)
+
+    def test6(self):
+        L = (1, 2, 3, 4, 5, 6, 7)
+        self.assertEqual(binarne_rek(L, 0, 6, 5), 4)
+
+    def test7(self):
+        L = (1, 2, 3, 4, 5, 6, 7)
+        self.assertEqual(binarne_rek(L, 0, 6, 6), 5)
+
+    def test8(self):
+        L = (1, 2, 3, 4, 5, 6, 7)
+        self.assertEqual(binarne_rek(L, 0, 6, 7), 6)
+
+    def test9(self):
+        L = (1, 2, 3, 4, 5, 6, 7)
+        self.assertEqual(binarne_rek(L, 0, 6, 8), None)
+
 
 if __name__ == "__main__":
     unittest.main()
--- zestaw12/zadanie3.py
+++ zestaw12/zadanie3.py
@@ -3,24 +3,50 @@ import unittest
 
 def mediana_sort(L, left, right):
     sort = sorted(L)
-    length = right - left
-    mid = int(left + right/2)
+    length = right - left + 1
+    mid = int(length / 2) + left
+    if length == 1:
+        return sort[left]
     if not length % 2:
-       return sort[mid] #wybieram element środkowy
+        x = sort[mid]
+        y = sort[mid-1]
+        return (x + y) / 2
     else:
-       return sort[int(length/2)] #przyjmuję medianę dolną
- 
-   
+        return sort[mid]
+
+
 class AllTests(unittest.TestCase):
     def test1(self):
-        L = [5, 2, 4, 3, 1]
-        self.assertEqual(mediana_sort(L, 0, 4), 3)
+        L = [5, 2, 4, 3, 1, 6]
+        self.assertEqual(mediana_sort(L, 2, 5), 4.5)
+    def test11(self):
+        L = [5, 2, 4, 3, 1, 6]
+        self.assertEqual(mediana_sort(L, 2, 2), 3)
+    def test12(self):
+        L = [5, 2, 4, 3, 1, 6]
+        self.assertEqual(mediana_sort(L, 2, 3), 3.5)
+    def test13(self):
+        L = [5, 2, 4, 3, 1, 6]
+        self.assertEqual(mediana_sort(L, 2, 4), 4)
+    def test14(self):
+        L = [5, 2, 4, 3, 1, 6]
+        self.assertEqual(mediana_sort(L, 1, 4), 3.5)
+
     def test2(self):
         L = [5, 8, -1, 6, 6, 1, 10]
         self.assertEqual(mediana_sort(L, 0, 6), 6)
+
     def test3(self):
         L = [6, 8, 3, 4, 9, 2]
-        self.assertEqual(mediana_sort(L, 0, 5), 4)
+        self.assertEqual(mediana_sort(L, 0, 5), 5)
+
+    def test4(self):
+        L = [2]
+        self.assertEqual(mediana_sort(L, 0, 0), 2)
+
+    def test5(self):
+        L = [6, 8]
+        self.assertEqual(mediana_sort(L, 0, 1), 7)
 
 
 if __name__ == "__main__":
